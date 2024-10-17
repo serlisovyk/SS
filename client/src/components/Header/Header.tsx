540a81f43672d785f4ff3554b3970a8e6681ff8b
Serhii Lisovyk <serlesovik@gmail.com>
2024-10-17 10:44:42 +0300

    Added input to Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import React, { ChangeEvent, useState } from 'react'
 import styles from './Header.module.css'
 import logo from './logo.svg'
 import search from './search.svg'
@@ -5,6 +6,13 @@ import heart from './heart.svg'
 import cart from './cart.svg'
 
 export default function Header() {
+  const [isSearchActive, setIsSearchActive] = useState(false)
+  const [searchActive, setSearchActive] = useState('')
+
+  const handleSearchClick = () => setIsSearchActive(prevState => !prevState)
+  const handleChangeSearchValue = (e: ChangeEvent<HTMLInputElement>) =>
+    setSearchActive(e.target.value)
+
   return (
     <div className={styles.header}>
       <div className="container">
@@ -20,22 +28,46 @@ export default function Header() {
           <div className={styles.login}>Вход / Регистрация</div>
         </div>
         <div className={styles.content}>
-          <div className={styles.logo}>
-            <img src={logo} alt="Logo" />
+          <div className={styles.logoSearchWrapper}>
+            <div className={styles.logo}>
+              <img src={logo} alt="Logo" />
+            </div>
+
+            {isSearchActive && (
+              <form>
+                <label htmlFor="search"></label>
+                <input
+                  type="text"
+                  id="search"
+                  name="search"
+                  value={searchActive}
+                  onChange={handleChangeSearchValue}
+                  className={styles.searchInput}
+                  placeholder="Поиск по каталогу товаров..."
+                />
+              </form>
+            )}
           </div>
-          <ul className={styles.contentList}>
-            <li className={styles.contentListItem}>
-              <span></span>
-              <span></span>
-              Каталог
-            </li>
-            <li className={styles.contentListItem}>Мужские</li>
-            <li className={styles.contentListItem}>Женские</li>
-            <li className={styles.contentListItem}>Детские</li>
-            <li className={styles.contentListItem}>Распродажа</li>
-          </ul>
+
+          {!isSearchActive && (
+            <ul className={styles.contentList}>
+              <li className={styles.contentListItem}>
+                <span></span>
+                <span></span>
+                Каталог
+              </li>
+              <li className={styles.contentListItem}>Мужские</li>
+              <li className={styles.contentListItem}>Женские</li>
+              <li className={styles.contentListItem}>Детские</li>
+              <li className={styles.contentListItem}>Распродажа</li>
+            </ul>
+          )}
+
           <div className={styles.links}>
-            <div className={styles.link}>
+            <div
+              className={`${styles.link} ${isSearchActive ? styles.activeLink : ''}`}
+              onClick={handleSearchClick}
+            >
               <img src={search} alt="Search" />
             </div>
             <div className={styles.link}>
