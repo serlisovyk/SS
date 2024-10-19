928437db39b70ed4e79cab7e1d04941cb78e2f60
Mark <mserous@gmail.com>
2024-10-19 19:22:45 +0200

    modified Products Model

--- server/models/ProductsModel.js
+++ server/models/ProductsModel.js
@@ -1,40 +1,33 @@
 const mongoose = require('mongoose');
 const Category = require('./CategoriesModel')
 
-const productSchema = new mongoose.Schema({
-    title: {type: String,  required: true},
-    price: {type: Number, required: true},
-    description: {type: String, required: true},
-    images:[{
-        type: String,
-        required: true
-    }],
-    category: [{type: String, required: true, ref: 'Category'}],
-    details: {
-        type: {
-            size: [{
-                type: Number,
-                unique: true,
-                required: true,
-            }],
-            color: [{
-                type: {
-                    name: {
-                        type: String,
-                        unique: true,
-                        required: true
-                    },
-                    value: {
-                        type: String,
-                        unique: true,
-                        required: true
-                    }
-                }
-            }]
-        }
-    }
+const mongoose = require('mongoose');
 
-})
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  images: [{
+    type: String,
+    required: true
+  }],
+  isCart: { type: Boolean, default: false },
+  isFavourite: { type: Boolean, default: false },
+  saleCF: { type: Number, default: 0 },
+  oldPrice: { type: Number },
+  colors: [{
+    name: { type: String, required: true },
+    hex: { type: String, required: true }
+  }],
+  sizes: [{
+    type: Number,
+    required: true
+  }],
+  material: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Product', productSchema);
+
 
 const Product = mongoose.model('Product', productSchema)
 module.exports = Product
